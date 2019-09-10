0ba80258a23abac1b8487fe6374a551d8df11813
hylian-flute <hylian_flute@yahoo.co.jp>
2019-09-10 06:07:29 +0000

    任意のファイルをアップデートするスクリプトを追加

---
+++ update.py
@@ -0,0 +1,18 @@
+from ftplib import FTP
+import getpass
+
+with FTP('users602.lolipop.jp') as ftp:
+  ftp.login('main.jp-charines', getpass.getpass('FTP Password: '))
+  file_list = [
+    'index.html',
+    #'help.html',
+    #'favicon.png',
+    #'scripts/script.js',
+    #'style/style.css',
+    #'data/items.json',
+    #'data/members.json'
+  ]
+  for path in file_list:
+    with open('./' + path, 'rb') as f:
+      ftp.storbinary('STOR ./girls-band-party/auto-party-v2/' + path, f)
+
