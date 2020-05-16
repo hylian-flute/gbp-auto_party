e08a79685a6ef340ad4ce4bbde76b03c8dfbb24c
hylian-flute <hylian_flute@yahoo.co.jp>
2020-05-16 15:25:28 +0900

    first commit in branch

---
+++ src/main.js
@@ -0,0 +1,204 @@
+"use strict";
+
+addEventListener("load", () => {
+  const worker = new Worker("./src/worker.js");
+  let workerProgress = 0;
+  worker.addEventListener("message", message => {
+    if (typeof message.data === "number") {
+      workerProgress += message.data;
+      console.log(`${Math.floor(100 * workerProgress)}%`);
+    } else {
+      console.dir(message.data);
+    }
+  });
+  worker.addEventListener("error", err => {
+    alert(err.message);
+  });
+
+  const types = ["パワフル", "クール", "ピュア", "ハッピー"];
+
+  const data = {
+    tabs: ["メンバー", "アイテム", "イベント", "結果"],
+    types: types,
+    bands: ["ポピパ", "Afterglow", "パスパレ", "Roselia", "ハロハピ", "モニカ"],
+    skills: ["条件付きスコアアップ", "スコアアップ", "その他"],
+    rarities: ["★", "★★", "★★★", "★★★★"],
+    sorts: ["キャラクター", "総合力", "最近の追加"],
+    characters: [
+      "戸山香澄", "花園たえ", "牛込りみ", "山吹沙綾", "市ヶ谷有咲",
+      "美竹蘭", "青葉モカ", "上原ひまり", "宇田川巴", "羽沢つぐみ",
+      "丸山彩", "氷川日菜", "白鷺千聖", "大和麻弥", "若宮イヴ",
+      "湊友希那", "氷川紗夜", "今井リサ", "宇田川あこ", "白金燐子",
+      "弦巻こころ", "瀬田薫", "北沢はぐみ", "松原花音", "奥沢美咲",
+      "倉田ましろ", "桐ケ谷透子", "広町七深", "二葉つくし", "八潮瑠唯",
+    ],
+    eventTypes: [...types, "なし"],
+    parameters: ["パフォーマンス", "テクニック", "ビジュアル"],
+    areas: [
+      "スタジオ(マイク)", "スタジオ(ギター)", "スタジオ(ベース)", 
+      "スタジオ(ドラム)", "スタジオ(その他)",
+      "ポスター", "カウンター", "ミニテーブル", "マガジンラック",
+      "入り口前", "看板", "センタースペース", "中庭", "おすすめメニュー",
+    ],
+    model: {
+      tab: 0,
+      filterTypes: (new Array(4)).fill(true),
+      filterBands: (new Array(6)).fill(true),
+      filterSkills: (new Array(3)).fill(true),
+      filterRarities: (new Array(4)).fill(true),
+      sort: 0,
+    },
+    members: [],
+    items: [],
+    event: {
+      //type: 4,
+      //characters: (new Array(AutoParty.CHARACTER_NUM)).fill(false),
+      //parameter: 3,
+      type: Math.floor(5 * Math.random()),
+      characters: [...new Array(AutoParty.CHARACTER_NUM)].map(_ => Math.random() < 1 / 6),
+      parameter: Math.floor(3 * Math.random()),
+    }
+  };
+
+  const methods = {
+    calculate: function() {
+      workerProgress = 0;
+      worker.postMessage({
+        members: this.members.filter(member => member.model.available),
+        items: this.items.filter(item => item.model.available),
+        event: this.event,
+      });
+    },
+    allMemberAvailable: function(available) {
+      this.filterdMembers.forEach(member => member.model.available = available);
+    },
+    allSkillIncrease: function(value) {
+      this.filterdMembers.forEach(member => {
+        const max = member.scoreUpTimeArr.length;
+        const level = (() => {
+          const level = parseInt(member.model.skillLevel, 10) + value;
+          if (level < 1) return 1;
+          if (level > max) return max;
+          return level;
+        })();
+        member.model.skillLevel = level.toString();
+      });
+    },
+    // 小数点以下の桁数を指定して数字を整形する
+    formatNum: function(num, digit) {
+      if (digit === 0) return Math.round(num).toString();
+
+      const int = Math.round(num * 10 ** digit).toString();
+      const left = int.length > digit ? int.slice(0, -digit) : "0";
+      const right = int.slice(-digit);
+      return `${left}.${right}`;
+    },
+    allItemAvailable: function(available) {
+      this.items.forEach(item => item.model.available = available);
+    },
+    allLevelIncrease: function(value) {
+      this.items.forEach(item => {
+        const max = item.paraUpRateArr.length;
+        const level = (() => {
+          const level = parseInt(item.model.level, 10) + value;
+          if (level < 1) return 1;
+          if (level > max) return max;
+          return level;
+        })();
+        item.model.level = level.toString();
+      });
+    },
+    getTarget: function(item) {
+      const [kind, value] = item.target;
+      if (kind === "band") return this.bands[value];
+      if (kind === "type") return this.types[value];
+      if (kind === "parameter") return this.parameters[value];
+      return "全て";
+    },
+  };
+
+  const computed = {
+    filterdMembers: function() {
+      const members = this.members.filter(member => (
+        this.model.filterTypes[member.type]
+        && this.model.filterBands[member.band]
+        && this.model.filterSkills[member.skill]
+        && this.model.filterRarities[member.rarity]
+      ));
+
+      const getCharacter = member => member.character;
+      const getMinusParameter = member => -member.totalParameter;
+      const getMinusId = member => -member.id;
+      const getType = member => member.type;
+      const generateCallback = (...funcs) => {
+        return (a, b) => {
+          for (const func of funcs) {
+            const diff = func(a) - func(b);
+            if (diff !== 0) return diff;
+          }
+          return 0;
+        };
+      };
+
+      if (this.model.sort === 0) {
+        members.sort(generateCallback(
+          getCharacter, getMinusParameter, getType, getMinusId
+        ));
+      } else if (this.model.sort === 1) {
+        members.sort(generateCallback(
+          getMinusParameter, getCharacter, getType, getMinusId
+        ));
+      } else {
+        members.sort(generateCallback(getMinusId));
+      }
+      return members;
+    },
+    itemAreas: function() {
+      const areaMap = new Map();
+      this.items.forEach(item => {
+        if (!areaMap.has(item.area)) areaMap.set(item.area, []);
+        areaMap.get(item.area).push(item);
+      });
+      const areas = [...areaMap.entries()]
+        .sort((a, b) => a[0] - b[0])
+        .map(pair => {
+          pair[1].sort((a, b) => a.target[1] - b.target[1]);
+
+          const oneline = 4;
+          const areaRows = [];
+          for (let i = 0; i < pair[1].length; i += oneline) {
+            areaRows.push(pair[1].slice(i, i + oneline));
+          }
+          return {area: pair[0], rows: areaRows};
+        });
+      return areas;
+    },
+  };
+
+  new Vue({
+    el: "#app",
+    data: data,
+    methods: methods,
+    computed: computed,
+  });
+
+  const fetchMembers = async () => {
+    return fetch("./data/members.json")
+      .then(res => res.json())
+      .then(memberObjs => memberObjs.map(obj => new AutoParty.Member(obj)))
+      .catch(err => alert(err.message));
+  };
+
+  const fetchItems = async () => {
+    return fetch("./data/items.json")
+      .then(res => res.json())
+      .then(itemObjs => itemObjs.map(obj => new AutoParty.Item(obj)))
+      .catch(err => alert(err.message));
+  }
+
+  Promise.all([fetchMembers(), fetchItems()])
+    .then(([members, items]) => {
+      data.members = members;
+      data.items = items;
+    });
+});
---
+++ src/worker.js
@@ -0,0 +1,222 @@
+"use strict";
+
+importScripts("./share.js");
+const MUSIC_LEN = 101;
+const ItemSet = class extends Array {
+  constructor(band, type, parameter) {
+    super();
+    this.band = band;
+    this.type = type;
+    this.parameter = parameter;
+    this.bandRate = undefined;
+    this.typeRate = undefined;
+    this.parameterRate = undefined;
+    this.anyRate = undefined;
+  }
+
+  // AND条件のエリアアイテムが実装された場合は変更が必要
+  calcSumRate() {
+    [this.bandRate, this.typeRate, this.parameterRate, this.anyRate]
+      = this.reduce((rates, item) => {
+      if (item.band !== null) rates[0] += item.paraUpRate;
+      else if (item.type !== null) rates[1] += item.paraUpRate;
+      else if (item.parameter !== null) rates[2] += item.paraUpRate;
+      else rates[3] += item.paraUpRate;
+      return rates;
+    }, [0, 0, 0, 0]);
+  }
+};
+
+const Event = class {
+  constructor({type, characters, parameter}) {
+    this.type = type;
+    this.parameter = parameter;
+
+    // typeは 4: なし が存在するので5未満
+    if (!(
+      this.type >= 0 && this.type < 5
+      && this.parameter >= 0 && this.parameter < 3
+      && Array.isArray(characters) && characters.length === AutoParty.CHARACTER_NUM
+      && characters.every(v => typeof v === "boolean")
+    )) throw new Error("Event.constructor: 不正なプロパティ");
+
+    this.characters = new Set();
+    characters.forEach((v, i) => {
+      if (v) this.characters.add(i);
+    });
+  }
+}
+
+addEventListener("message", message => {
+  const members = message.data.members.map(member => {
+    const obj = Object.create(AutoParty.Member.prototype);
+    for (const key in member) obj[key] = member[key];
+    obj.checkModel();
+    return obj;
+  });
+  const items = message.data.items.map(item => {
+    const obj = Object.create(AutoParty.Item.prototype);
+    for (const key in item) obj[key] = item[key];
+    obj.checkModel();
+    return obj;
+  });
+  const event = new Event(message.data.event);
+
+  const areas = [...new Array(14)].map(v => []);
+  items.forEach(item => areas[item.area].push(item));
+
+  const createItemSet = (band, type, parameter) => {
+    const itemSet = new ItemSet(band, type, parameter);
+    areas.forEach(area => {
+      // 現状同エリアで性質の違うアイテムは存在しないが
+      // バンドorタイプ系の倍率とパラメータ系の倍率を比較することは本来意味がない
+      const item = area.reduce((maxItem, item) => {
+        if (item.isApplicable(band, type, parameter)
+          && (maxItem === null || item.paraUpRate > maxItem.paraUpRate)) {
+          return item;
+        }
+        return maxItem;
+      }, null);
+      if (item !== null) itemSet.push(item);
+    });
+    itemSet.calcSumRate();
+    return itemSet;
+  };
+
+  //TODO: 「みんなの」系アイテムが最適解になるパターンがある
+  const itemSets = [];
+  for (let band = 0; band < AutoParty.BAND_NUM; ++band) {
+    for (let type = 0; type < 4; ++type) {
+      for (let parameter = 0; parameter < 3; ++parameter) {
+        itemSets.push(createItemSet(band, type, parameter));
+      }
+    }
+  }
+
+  const calcBuffedParameter = (member, itemSet) => {
+    const isEventCharacter = event.characters.has(member.character);
+    const isEventType = (event.type === member.type);
+    const isEventBoth = isEventCharacter && isEventType;
+    return member.totalParameter
+      * (1
+        + (member.band === itemSet.band ? itemSet.bandRate : 0)
+        + (member.type === itemSet.type ? itemSet.typeRate : 0)
+        + itemSet.anyRate
+        + (isEventCharacter ? 0.2 : 0)
+        + (isEventType ? 0.1 : 0)
+        + (isEventBoth ? 0.2 : 0)
+      )
+      + member.parameters[itemSet.parameter] * itemSet.parameterRate
+      + (isEventBoth ? 0.5 * member.parameters[event.parameter] : 0);
+  };
+
+  const filters = [
+    () => true,
+    (member, itemSet) => member.band === itemSet.band,
+    (member, itemSet) => member.type === itemSet.type,
+    (member, itemSet) =>
+      member.band === itemSet.band && member.type === itemSet.type
+  ];
+  const calcSkills = [
+    member => member.scoreUpRate * member.scoreUpTime,
+    member => (member.highCondition === 1
+      ? member.scoreUpRateHigh : member.scoreUpRate) * member.scoreUpTime,
+    member => (member.highCondition === 2
+      ? member.scoreUpRateHigh : member.scoreUpRate) * member.scoreUpTime,
+    member => (member.highCondition === 1 || member.highCondition === 2
+      ? member.scoreUpRateHigh : member.scoreUpRate) * member.scoreUpTime,
+  ];
+  const enumerateCombination = arr => {
+    const N = 5;
+    const result = [];
+
+    // stack[i] = [選択された要素の配列, 次に見る要素のインデックス]
+    const stack = [[[], 0]];
+
+    while (stack.length > 0) {
+      const pair = stack.pop();
+      if (pair[0].length === N) result.push(pair[0]);
+      else if (arr.length - pair[1] < N - pair[0].length) continue;
+      else {
+        stack.push([pair[0], pair[1] + 1]);
+        stack.push([[...pair[0], arr[pair[1]]], pair[1] + 1]);
+      }
+    }
+
+    return result;
+  };
+
+  const result = itemSets.reduce((result, itemSet) => {
+    const memberPowers = members.map(member => ({
+      member: member,
+      parameter: calcBuffedParameter(member, itemSet),
+    })).sort((a, b) => b.parameter - a.parameter);
+
+    let resultOfItemSet = {total: 0};
+    // filterIdx === 0: 条件なし, 1: バンド統一, 2: タイプ統一, 3: 両方統一
+    for (let filterIdx = 0; filterIdx < 4; ++filterIdx) {
+      const memberBenefits = memberPowers.reduce((memberBenefits, memberPower) => {
+        if (filters[filterIdx](memberPower.member, itemSet)) {
+          return [
+            ...memberBenefits,
+            {...memberPower, skill: calcSkills[filterIdx](memberPower.member)}
+          ];
+        } else return memberBenefits;
+      }, [])
+
+      const reducedMemberBenefits
+        = memberBenefits.filter((memberBenefit, memberIdx) => {
+        const characterSet = new Set();
+        for (const comparison of memberBenefits.slice(0, memberIdx)) {
+          if (comparison.skill >= memberBenefit.skill) {
+            if (comparison.member.character === memberBenefit.member.character) {
+              return false;
+            } else {
+              characterSet.add(comparison.member.character);
+              if (characterSet.size >= 5) return false;
+            }
+          }
+        }
+        return true;
+      });
+
+      const resultOfFilterIdx
+        = enumerateCombination(reducedMemberBenefits).reduce((result, party) => {
+
+        if ((new Set(
+          party.map(memberBenefit => memberBenefit.member.character)
+        )).size < 5) {
+          return result;
+        }
+
+        party.sort((a, b) => b.skill - a.skill);
+
+        const [totalParameter, totalSkill] = party.reduce(
+          (pair, memberBenefit) => [pair[0] + memberBenefit.parameter, pair[1] + memberBenefit.skill],
+          [0, party[0].skill]
+        );
+        const skillPower = totalParameter * totalSkill / MUSIC_LEN;
+        const total = totalParameter + skillPower;
+
+        if (total > result.total) return {
+          total: total,
+          totalParameter: totalParameter,
+          skillPower: skillPower,
+          members: party.map(memberBenefit => memberBenefit.member),
+        };
+        return result;
+      }, {total: 0});
+
+      if (resultOfFilterIdx.total > resultOfItemSet.total) {
+        resultOfItemSet = resultOfFilterIdx;
+      }
+    }
+
+    postMessage(1 / itemSets.length);
+
+    if (resultOfItemSet.total > result.total) return resultOfItemSet;
+    return result;
+  }, {total: 0});
+
+  postMessage(result);
+});
